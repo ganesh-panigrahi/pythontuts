46d58cbcc58c88150af4490afcc49e496e4b370c
Ganesh Panigrahi <ganeshpanigrahi488@gmail.com>
2022-12-27 11:56:52 +0530

    tut10,12 files committed

---
+++ tut10.py
@@ -0,0 +1,7 @@
+# Dictionary - a pair of key (word) and values (meaning)
+d1 = {}   # a blank dictionary named as "d1"
+print(type(d1))
+# d1 = ()  # a blank tuple
+# print(type(d1))
+d2 = {"Harry":"Burger", "Rohan":"Fish", "SkillF":"Roti"}
+print(d2)
